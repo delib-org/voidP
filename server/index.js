4296f565655e259eaf0ff9f05e052223a50b14f4
Tal Yaron <tal.yaron@gmail.com>
2020-03-12 13:38:01 +0200

    we have recursion of levels

--- server/index.js
+++ server/index.js
@@ -1,46 +1,50 @@
 class MO {
-    constructor(output, id, input1, input2){
+    constructor(output, id, input1, input2) {
         this.output = output;
         this.id = id;
         this.input1 = input1;
         this.input2 = input2;
     }
-    
+
 }
 
-const input = [1, 0, 0, 1,1,1];
+const input = [1, 0, 0, 1, 1, 1, 0, 1];
 const inputMOLayer = [];
-input.map((mo,index)=>{
-    inputMOLayer[index] = new MO(null, `0${index}`,input[index], null);
+
+
+
+
+
+let layers = [];
+let counter = 0;
+
+
+
+input.map((mo, index) => {
+    inputMOLayer[index] = new MO(null, `${counter}-${index}`, input[index], null);
 })
 
+layers[counter] = inputMOLayer;
 
 
-let inputLayer = inputMOLayer;
-let layers = [[],[]];
-let counter = 1;
-let counter1 = 0;
-layers[0] = inputLayer;
+while (layers[counter].length > 2) {
+    counterLoop = 0
 
-while(inputLayer.length>=2){
-    //get a couple and pair them
-    let couple = inputLayer.slice(0,2)
-   
-    layers[counter].push(new MO(null,`1${counter1}`,couple[0].id, couple[1].id));
-    couple[0].output = `1${counter1}`;
-    couple[1].output = `1${counter1}`;
-    inputLayer = inputLayer.slice(2);
-    counter1++;
-    
+    layers.push([]);
+    while (counterLoop < layers[counter].length) {
+        //get a couple and pair them
+        let couple = layers[counter].slice(counterLoop, counterLoop + 2);
+
+
+        couple[0].output = `${counter}-${counterLoop}`;
+        couple[1].output = `${counter}-${counterLoop}`;
+        layers[counter + 1].push(new MO(null, `${counter+1}-${counterLoop}`, couple[0].id, couple[1].id));
+
+        counterLoop += 2;
+
+    }
+    counter++;
+    console.log(layers)
 }
-console.log(layers)
 
-if(inputLayer.length>2){
 
-}
-let network = {
-    layer1:
-        [
-
-        ]
-}
